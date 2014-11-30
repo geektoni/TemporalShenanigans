1b6b120e70bf6f47dede87c6d9cb169a51d22c92
Giovanni De Toni <giovanni@detoni.me>
2014-11-30 19:00:02 +0100

    Errata Inteface renamed in Interface

---
+++ Interface/main.cc
@@ -0,0 +1,14 @@
+#include <ncurses.h>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include "Interface.h"
+using namespace std;
+
+int main (int argc, char * argv[]) {
+
+	Interface screen;
+	screen.printWindow();
+
+return 0;
+}
